bcc0a5a09cefc489f9793bff4a5be252074a74f6
Damjan Savic <starconfig09@gmail.com>
2024-11-14 21:11:33 +1100

    updates

--- public/js/dam-slideshow-public.js
+++ public/js/dam-slideshow-public.js
@@ -1,1 +1,28 @@
- 
+ /**
+ * File: /dam-slideshow/public/js/dam-slideshow-public.js
+ */
+(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+        const slideshows = document.querySelectorAll('.dam-slideshow');
+        
+        slideshows.forEach(slideshow => {
+            new Swiper(slideshow, {
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                },
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false
+                }
+            });
+        });
+    });
+})(jQuery);
--- public/partials/dam-slideshow-public-display.php
+++ public/partials/dam-slideshow-public-display.php
@@ -1,1 +1,24 @@
- 
+ /**
+ * File: /dam-slideshow/public/partials/dam-slideshow-public-display.php
+ */
+?>
+<div class="dam-slideshow swiper">
+    <div class="swiper-wrapper">
+        <?php foreach ($attributes['slides'] as $slide): ?>
+            <div class="swiper-slide" style="background-image: url(<?php echo esc_url($slide['backgroundImage']); ?>)">
+                <div class="slide-content">
+                    <h1><?php echo esc_html($slide['heading']); ?></h1>
+                    <p><?php echo esc_html($slide['paragraph']); ?></p>
+                    <?php if (!empty($slide['buttonText']) && !empty($slide['buttonUrl'])): ?>
+                        <a href="<?php echo esc_url($slide['buttonUrl']); ?>" class="slide-button">
+                            <?php echo esc_html($slide['buttonText']); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+</div>
